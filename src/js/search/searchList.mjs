97913062f9c373ec46786daba3ee7421d0b6a0e3
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-09-12 15:38:48 +0200

    Add search function

--- src/js/search/searchList.mjs
+++ src/js/search/searchList.mjs
@@ -6,5 +6,26 @@ export function searchAuctions () {
 
     searchInput.addEventListener("keyup", async e => {
         const itemData = await getAuctionsList(); 
+        let currentValue = e.target.value.toLowerCase();
+        let matchedItems = itemData.filter(function(item) {
+            const itemTitle = itemData.title.toLowerCase();
+            return itemTitle.startsWith(currentValue) || itemTitle.includes(currentValue);
+        })
     })
+
+    showSearchResult(matchedItems); 
+}
+
+function showSearchResult(results) {
+
+
+    const auctionsContainer = document.querySelector(".listingContainer");
+    if (results.length === 0) {
+        const noResults = document.createElement("div");
+        noResults.textContent = "No matches";
+        auctionsContainer.appendChild(noResults); 
+    } else {
+        
+    }
+
 }
